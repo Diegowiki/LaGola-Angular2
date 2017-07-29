01ea7b87acbc96f01c818dc9838fe5168deaf278
DiegoWiki <diegocorral.del@gmail.com>
2017-07-29 17:27:03 +0200

    upload favicon, logo pictures. Style navbar header. Fixed Router imports on app.component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { NavigationEnd, Route, Router } from '@angular/router';
+import { MzSidenavComponent } from 'ng2-materialize';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
