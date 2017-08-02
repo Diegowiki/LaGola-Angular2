d9f195eafa9c1b5e81a524b4300f7d9041c9953d
DiegoWiki <diegocorral.del@gmail.com>
2017-08-02 16:39:11 +0200

    map and text in contact page styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Route, Router } from '@angular/router';
 import { MzSidenavComponent } from 'ng2-materialize';
+// import {} from '@types/googlemaps';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { GoogleMapsAPIWrapper } from '@agm/core';
+
+declare var google: any;
+
 
 @Component({
   selector: 'app-contact',
@@ -6,8 +10,100 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
-  title = 'My first AGM project';
-  lat = 51.678418;
-  lng = 7.809007;
+  lat = 40.510124;
+  lng = -3.353459;
+  clickableIcons = false;
+  fullscreenControl	= true;
+  gestureHandling = 'auto';
+  keyboardShortcuts = false;
+  mapDraggable = true;
+  mapTypeControl = false;
+  mapTypeId = 'hybrid'; // 'roadmap'|'hybrid'|'satellite'|'terrain'|string = 'roadmap';
+  maxZoom = 20;
+  zoom = 18;
+  minZoom = 13;
+  rotateControl = false;
+  scrollwheel = false;
+  zoomControl = true;
+  disableDefaultUI = true;
+  /*zoomControlOptions = {
+    position: google.maps.ControlPosition.LEFT_CENTER
+  };*/
+
+  MapTypeStyles: any[] = [
+    { elementType: 'geometry', stylers: [{ color: '#343F48' }] },
+    { elementType: 'labels.text.stroke', stylers: [{ color: '#343F48' }] },
+    { elementType: 'labels.text.stroke', stylers: [{ color: '#D6D4C2' }] },
+    {
+      featureType: 'administrative.locality',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#343F4A' }]
+    },
+    {
+      featureType: 'poi',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#343F48' }, {visibility: 'simplified'}]
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'geometry',
+      stylers: [{ color: '#4db6ac' }]
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#6b9a76' }]
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry',
+      stylers: [{ color: '#D6D4C2' }]
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry.stroke',
+      stylers: [{ color: '#D6D4C2' }]
+    },
+    {
+      featureType: 'road',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#343F48' }]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry',
+      stylers: [{ color: '#746855' }]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry.stroke',
+      stylers: [{ color: '#D6D4C5' }]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#f3d19c' }]
+    },
+    {
+      featureType: 'water',
+      elementType: 'geometry',
+      stylers: [{ color: '#0097a7' }]
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#515c6d' }]
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.stroke',
+      stylers: [{ color: '#17263c' }]
+    },
+    {
+      featureType: 'administrative.land_parcel',
+      elementType: 'geometry.fill',
+      stylers: [{ color: '#4db6ac' }]
+    }
+  ];
 
 }
