66bf126cc727272cfb13eadb327320fb3a8f4cc4
DiegoWiki <diegocorral.del@gmail.com>
2017-08-03 11:55:12 +0200

    Carta component addapted for database imp

--- src/app/carta/Carta.ts
+++ src/app/carta/Carta.ts
@@ -1,11 +1,7 @@
 export class Carta {
   constructor(
     public title: string,
-    public header1: string,
-    public primeros: string[],
-    public header2: string,
-    public segundos: string[],
-    public included: string
+    public secciones: [{header: string, body: string[]}]
     ) { }
 }
 
--- src/app/carta/carta.component.ts
+++ src/app/carta/carta.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Carta } from './carta';
 
 @Component({
@@ -6,20 +6,17 @@ import { Carta } from './carta';
   templateUrl: './carta.component.html',
   styleUrls: ['./carta.component.scss'],
 })
-export class CartaComponent {
-  menus = [
-    new Carta('Menú diario',
-    'Primeros',
-    ['Ensalada de pasta', 'Crema de setas', 'Revuelto de morcilla', 'Gazpacho'],
-    'Segundos',
-    ['Costillas ibericas', 'Alitas fritas con salsa picante', 'Chanquetes'],
-    'Postre, pan y bebida incluidos'),
-    new Carta('Menú Especial',
-    'Primeros',
-    ['Paella de marisco', 'Pimiento relleno de queso y cangrejo', 'Ensalada de pollo y manzana', 'Gazpacho'],
-    'Segundos',
-    ['Pierna de cordero asada', 'Entrecot con pimiento de padrón', 'Bacalao a la riojana'],
-    'Postre, pan y bebida incluidos')
-  ];
+export class CartaComponent implements OnInit {
+  public Cartas: Carta[];
+  public Carta: Carta;
+  public seccion: {header: string, body: string[]};
+
+  ngOnInit() {
+    this.Carta.title = 'Carta';
+    this.seccion.header = 'asdsad';
+    this.seccion.body = ['sdasd', 'asdasd'];
+    this.Carta.secciones.push(this.seccion);
+    this.Cartas.push(this.Carta);
+  }
 
 }
